fead71a357aa0678f658efa777975667cc4c1eb9
Officer-Erik-1K-88 <oficer.erik.k@gmail.com>
2024-09-19 14:38:46 -0500

    Documentation format update

--- Documentation/resources/file_script.js
+++ Documentation/resources/file_script.js
@@ -1,4 +1,21 @@
 window.onload = function() {
+    try {
+        const extend = document.getElementsByClassName("classExtends")[0];
+        const implement = document.getElementsByClassName("classImplements")[0];
+        if (extend.innerHTML != "") {
+            extend.innerHTML = "<b>Extends</b>: "+extend.innerHTML;
+        }
+
+        if (implement.innerHTML != "") {
+            implement.innerHTML = (extend.innerHTML != ""?"<b>&</b> ":"")+"<b>Implements</b>: "+implement.innerHTML;
+            const content = implement.innerHTML;
+            if (content.split(",").length>1) {
+                var finalCom = content.lastIndexOf(",");
+                implement.innerHTML = content.slice(0, finalCom) + ", <b>&</b> " + content.slice(finalCom+1);
+            }
+        }
+    } catch(Error) {}
+
     var height = document.body.scrollHeight;
     if (height < 56) {
         height += 56;
